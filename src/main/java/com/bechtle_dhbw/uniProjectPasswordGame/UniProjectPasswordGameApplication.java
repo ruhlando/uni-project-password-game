741788784b75c62340dbc74b7ecde779da19fbd6
maurizze004 <baume2004@gmail.com>
2024-06-24 12:59:31 +0200

    project set up

---
+++ src/main/java/com/bechtle_dhbw/uniProjectPasswordGame/UniProjectPasswordGameApplication.java
@@ -0,0 +1,13 @@
+package com.bechtle_dhbw.uniProjectPasswordGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniProjectPasswordGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniProjectPasswordGameApplication.class, args);
+	}
+
+}
